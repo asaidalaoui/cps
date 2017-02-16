9f8ccf78d696a226ecca583bf7a5fe89b39d77ad
pablo <pablo0168@hotmail.com>
2017-02-15 21:36:10 -0600

    cleanup code of old comments and added a few more, we still need to add error checking for the input in keyboard.py

--- src/race/src/control.py
+++ src/race/src/control.py
@@ -8,74 +8,71 @@ import time
 kp = .8
 kd = .8
 servo_offset = 18.5
-prev_error = 0.0 
+prev_error = 0.0
 vel_input = 50
+n = 0
+angle = 0
 
 pub = rospy.Publisher('drive_parameters', drive_param, queue_size=1)
 
+'''
+    the on(n) and off(n) functions take a parameter 0 <= n <= 50
+    and linearly approximate a velocity using a linear regression
+    where the value n = 1 represents the lowest on/off time ratio
+    that allows the car to move consistently and reliably.
 
+    as n approaches 50, less time is spent sleeping with the motor off
+    and more time sleeping with the motor on.
+
+'''
+# calculate time on while the motor is powered
 def on(n):
-    return 0.001429 * n + 0.06857
+	if (n == 0):
+		return 0
+	else:
+		return 0.001429 * n + 0.06857
 
+# calculate time off while motor is not powered
 def off(n):
 	if (n == 50):
-		return 0	
+		return 0
+	elif (n == 0):
+		return 1
+	else:
+        return -0.001429*n + 0.07143
 
-    	return -0.001429*n + 0.07143
+def stateUpdater(data):
+	global n
+	global angle
+	print ("state updater")
+	n = data.velocity
+	angle = data.angle
 
+def control():
 
-
-def control(data):
 	global prev_error
 	global vel_input
 	global kp
 	global kd
-
-	## Your code goes here
-	# 1. Scale the error
-	# 2. Apply the PID equation on error
-	# 3. Make sure the error is within bounds
-#	angle = data.pid_error*kp;
-#	if angle > 100:
-#		angle = 100
-#	if angle < -100:
-#		angle = -100
-
-	# circle parameters :
-	# sleep while running = .08
-	# sleep while stopped = .1
-	# angle = 100 / -100
-
-	# slow straighline parameters:
-	# sleep while running = .07
-	# sleep while stopped = .07
-	# angle = 16.5 
+	global n
+	global angle
 
 	msg = drive_param();
 
-	n = 49
-
-	msg.angle = 4	
-#	msg.velocity = 50
-#	pub.publish(msg)
 	while not rospy.is_shutdown():
+		msg.angle = angle
 		msg.velocity = 10
 		pub.publish(msg)
 		# sleep motor on
 		time.sleep(on(n))
 
-		msg.velocity = 0 
+		msg.velocity = 0
 		pub.publish(msg)
 		# sleep motor off
 		time.sleep(off(n))
-		
-
-	#if(data.pid_vel == 0):
-	#	msg.velocity = -8
-	#else:
-	#	msg.velocity = vel_input	
 
 if __name__ == '__main__':
 	rospy.init_node('pid_controller', anonymous=True)
-	rospy.Subscriber("error", pid_input, control)
+	rospy.Subscriber("UI_drive_parameters", drive_param, stateUpdater)
+	control()
 	rospy.spin()
--- src/race/src/keyboard.py
+++ src/race/src/keyboard.py
@@ -4,76 +4,57 @@ import rospy
 from race.msg import drive_param
 import curses
 #import signal
-#TIMEOUT = 0.1 # number of seconds your want for timeout
+
+# speed and angle
 forward = 0;
 left = 0;
 
-# def interrupted(signum, frame):
-#     "called when read times out"
-#     global forward
-#     forward = 0
-#     global left
-#     left = 0
-#     stdscr.addstr(2, 20, "Stop")
-#     stdscr.addstr(2, 25, '%.2f' % forward)
-#     stdscr.addstr(3, 20, "Stop")
-#     stdscr.addstr(3, 25, '%.2f' % left)
-# signal.signal(signal.SIGALRM, interrupted)
-
-# def input():
-#     try:
-#             foo = stdscr.getch()
-#             return foo
-#     except:
-#             # timeout
-#             return
-
+# curses window object
 stdscr = curses.initscr()
 curses.cbreak()
 stdscr.keypad(1)
 rospy.init_node('keyboard_talker', anonymous=True)
-pub = rospy.Publisher('drive_parameters', drive_param, queue_size=10)
-
-# set alarm
-#signal.alarm(TIMEOUT)
-#s = input()
-# disable the alarm after success
-#signal.alarm(0)
-#print 'You typed', s
+pub = rospy.Publisher('UI_drive_parameters', drive_param, queue_size=10)
 
 stdscr.refresh()
 
 key = ''
+param = ""
+
 while key != ord('q'):
-#	signal.setitimer(signal.ITIMER_REAL,0.05)
-#	key = input()
-	key = stdscr.getch()
+	stdscr.clrtoeol()
 	stdscr.refresh()
-#	signal.alarm(0)
-	if key == curses.KEY_UP: 
-		forward = forward + 0.1;
-		stdscr.addstr(2, 20, "Up  ")
-		stdscr.addstr(2, 25, '%.2f' % forward)
-		stdscr.addstr(5, 20, "    ")
-	elif key == curses.KEY_DOWN:
-		forward = forward - 0.1; 
-		stdscr.addstr(2, 20, "Down")
-		stdscr.addstr(2, 25, '%.2f' % forward)
-		stdscr.addstr(5, 20, "    ")
-	if key == curses.KEY_LEFT:
-		left = left - 0.1; 
-		stdscr.addstr(3, 20, "left")
-		stdscr.addstr(3, 25, '%.2f' % left)
-		stdscr.addstr(5, 20, "    ")
-	elif key == curses.KEY_RIGHT:
-		left = left + 0.1; 
-		stdscr.addstr(3, 20, "rgt ")
-		stdscr.addstr(3, 25, '%.2f' % left)
-		stdscr.addstr(5, 20, "    ")
-	if key == curses.KEY_DC:
-		left = 0
-		forward = 0
-		stdscr.addstr(5, 20, "Stop")
+	stdscr.addstr(2, 20, "Type s to select speed (0 to 50)\n")
+	stdscr.addstr(5,20, "Type t to select angle (-100 to 100)\n")
+	stdscr.addstr(8,20, "Type c to run in a circle\n")
+
+	key = stdscr.getch()
+	if key == ord('s'):
+		stdscr.clrtoeol()
+		stdscr.refresh()
+
+		stdscr.addstr(2, 20, "Type s to select speed (0 to 50)\n")
+		stdscr.addstr(5,20, "Type t to select angle (-100 to 100)\n")
+		stdscr.addstr(8,20, "Type c to run in a circle\n")
+
+		stdscr.addstr(11,20,"Enter the speed: ")
+		param = stdscr.getstr()
+		forward = int(param)
+
+	elif key == ord('t'):
+		stdscr.clrtoeol()
+		stdscr.refresh()
+		stdscr.addstr(2,20, "Enter the angle: ")
+		param = stdscr.getstr()
+		left = int(param)
+
+	elif key == ord('c'):
+		forward = 1
+		left = 100
+
+	else:
+		continue
+
 	msg = drive_param()
 	msg.velocity = forward
 	msg.angle = left
